162760adaa6ccd4a620a5b50130f6afc3fa80658
Elias Moubarak <elie00020@gmail.com>
2023-03-27 18:22:40 +0200

    from scratch

---
+++ serve-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+set -x
+
+# This script is executed whenever the docker container is (re)started.
+#===============================================================================
+bokeh serve Preset Upload Table Line Figure Adsorption  \
+    --port 5006                 \
+    --log-level debug           \
+    --allow-websocket-origin "*" \
+    --prefix "$BOKEH_PREFIX" \
+    --use-xheaders
+# --allow-websocket-origin discover.materialscloud.org
+# --allow-websocket-origin localhost:5006
+
+#===============================================================================
+
+#EOF
