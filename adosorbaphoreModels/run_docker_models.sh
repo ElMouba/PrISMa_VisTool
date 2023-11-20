8370b125c5412dbee84e918af2f77607cdd830d8
Elias Moubarak <58478634+ElMouba@users.noreply.github.com>
2023-11-20 11:19:32 +0100

    Delete adosorbaphoreModels directory

--- adosorbaphoreModels/run_docker_models.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "buildig image models-dev"
-docker build --tag models-dev .
-
-docker stop models-dev
-docker container prune
-
-echo "connect to http://localhost:8000"
-docker run --rm -p 8000:8000 -e PORT=8000 -e WORKERS=2 -e CONCURRENCY_LIMIT=2 --name=models-dev models-dev
