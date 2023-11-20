d16c3e24dbac08a151ff73fde2649c8ee85e7c76
Elias Moubarak <58478634+ElMouba@users.noreply.github.com>
2023-11-20 11:20:15 +0100

    Delete prisma-front directory

--- prisma-front/run_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t prisma-front .
-echo "connect to http://localhost:5006"
-docker run -d --rm -p 5006:5006 -e PORT=5006 -e WORKERS=2 -e CONCURRENCY_LIMIT=2 --name=prisma-front prisma-front
--- prisma-front/scripts/restart.sh
+++
@@ -1,5 +0,0 @@
-docker stop prisma-front
-docker container prune
-
-docker build -t prisma-front . --no-cache
-
