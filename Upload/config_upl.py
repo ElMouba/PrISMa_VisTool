18484fa7b10ee8004656eca2c98b3839b563fece
Joren <joren.vanherck@epfl.ch>
2024-06-25 15:03:11 +0200

    fix Upload

--- Upload/config_upl.py
+++ Upload/config_upl.py
@@ -4,3 +4,10 @@ WWIDTH = 300
 REQUIRED_FIELDS = ['FirstName', 'LastName', 'Email', 'Consent', 'CifName']
 
 SHARED_FOLDER = './Uploaded'
+
+WWIDTH = 300
+
+SENDER_EMAIL = 'prismastructures@gmail.com'
+SENDER_PASSWORD = 'ctjadksgrsjkabvf'
+
+ADMINS = ['joren.vanherck@epfl.ch']
